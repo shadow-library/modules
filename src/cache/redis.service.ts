152ae60adff2bb32013b32a9bd978089efdb5385
Leander Paul <leanderpaul97@outlook.com>
2025-11-22 15:31:24 +0400

    feat: cache module

---
+++ src/cache/redis.service.ts
@@ -0,0 +1,19 @@
+/**
+ * Importing npm packages
+ */
+import { Injectable } from '@shadow-library/app';
+
+/**
+ * Importing user defined packages
+ */
+
+/**
+ * Defining types
+ */
+
+/**
+ * Declaring the constants
+ */
+
+@Injectable()
+export class RedisService {}
